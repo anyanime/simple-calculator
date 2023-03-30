4867523e02470565065f04dafac4f6dcfa007bd0
Anyanime Benson <bensonanyanime@gmail.com>
2023-03-31 00:59:40 +0100

    Add state and functionality to App

--- src/App.js
+++ src/App.js
@@ -1,16 +1,57 @@
 import './index.css';
 import Frame from './components/Frame';
 import Output from './components/Output';
-import Button from './components/Button';
-
+import Button from './components/Button'
+import {useState} from 'react';
 
 
 function App() {
+  //Add state and functionality to the App
+  const [result, setResult] = useState("");
+  const handleClick = (e) => {
+    console.log(e)
+    const value = e;
+
+    if(e === "="){
+              calculate()
+          }       
+          else if(e === "C"){
+              reset()
+          }
+          else if(e === "del"){
+              backspace()
+          }
+      
+          else {
+              setResult(result + value)
+          }
+  }
+
+  const calculate = () => {
+        try {
+            setResult(
+                // eslint-disable-next-line
+                eval(result))
+        } catch (error) {
+            setResult({
+                result: error.name
+            })
+    
+        }
+    };
+    
+    const reset = () => {
+        setResult("")
+    };
+    
+    const backspace = () => {
+      setResult(result.slice(0, -1))
+    };
   
   return (
     <Frame>
-      <Output />
-      <Button />
+      <Output result={result}/>
+      <Button onClick={handleClick}/>
     </Frame>
     
   )
